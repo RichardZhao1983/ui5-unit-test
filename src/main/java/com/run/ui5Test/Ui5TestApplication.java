a78038f1ca2e93c4b9f4a05b362fffaf74e38d92
Zhao, Richard <richard.zhao@sap.com>
2018-09-19 15:16:15 +0800

    project inital

---
+++ src/main/java/com/run/ui5Test/Ui5TestApplication.java
@@ -0,0 +1,12 @@
+package com.run.ui5Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ui5TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ui5TestApplication.class, args);
+	}
+}
